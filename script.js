96881c012f33a0a1f792e1b0dcaea9a079b70d1f
Wendson <wendsonsantanajps@gmail.com>
2021-03-30 21:50:26 -0300

    Operações Aritmeticas

--- script.js
+++ script.js
@@ -7,3 +7,41 @@ var aulavar ;
 aulavar = "Teste" ;
 
 console.log(aulavar);
+
+
+//  \n Quebra linha na variavel ex: 
+
+var quebraLinha = "Linha um \n Linha Dois";
+
+console.log(quebraLinha);
+
+/* 
+
+    Boolean: True ou false ainda estudar sobre para fazer operações logicas
+    Null: Literalmente vazio, deixar a variavel vazio
+    Undefined: Quando não setamos o que é a variavel e o nosso sistema não sabe o que tem dentro
+    Number: Literalmente numero kk
+    String: Conjunto de caracteres se diz uma string aquilo que esta entre aspas duplas "String"
+
+*/
+
+/* 
+    Operações aritmeticas
+    Adição  +
+    Subtração -
+    Multiplicação *
+    Divisão / 
+    Módulo %
+    Incremento ++
+    Decremento --
+
+
+*/
+
+var OpArit = 5;
+var OpArit2 = 6;
+
+var OpAritRes = OpArit + OpArit2;
+
+console.log(OpAritRes)
+
